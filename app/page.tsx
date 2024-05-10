86b3a21a7746b6a5320a2913b73679e06a642822
PolCarva <pablocarvalhogimenez@gmail.com>
2024-05-10 17:04:35 -0300

    Add Footer component to Home page and Favicons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AboutSection from "@/components/AboutSection";
 import ClientsSection from "@/components/ClientsSection";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import PortfolioSection from "@/components/PortfolioSection";
@@ -16,6 +17,7 @@ export default function Home() {
         <TestimonialsSection />
         <ClientsSection />
         <PortfolioSection />
+        <Footer />
       </main>
     </>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+import React from 'react'
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from 'react-icons/fa6';
+
+
+
+const socials = [
+    {
+        icon: <FaFacebookF className='text-3xl' />,
+        href: "https://www.facebook.com/nicestream"
+    },
+    {
+        icon: <FaInstagram className='text-3xl' />,
+        href: "https://www.instagram.com/nicestream/?hl=es"
+    },
+    {
+        icon: <FaLinkedinIn className='text-3xl' />,
+        href: "https://www.linkedin.com/company/nicestreamdigital/"
+    },
+
+]
+const Footer = () => {
+    const currentYear = new Date().getFullYear()
+    return (
+        <footer className='h-32 flex flex-col justify-center items-center text-black gap-3'>
+            <p className='text-lg'>
+                © Todos los derechos reservados - {currentYear}. Desarrollado por {" "}
+                <Link className='text-primary-500 hover:text-primary-400 transition' href={"https://nicestream.com"}>
+                    Nicestream
+                </Link>
+            </p>
+            <div className='flex gap-5'>
+                {socials.map((item) => (
+                    <span className='relative w-8 h-8 group'>
+                        <Link key={item.href}
+                            className='absolute text-primary-500 group-hover:translate-x-[2px] group-hover:translate-y-[2px] footerSocialLink transition duration-300'
+                            href={item.href} target='_blank'>
+                            {item.icon}
+                        </Link>
+                        <Link key={item.href}
+                            className='absolute inset-0 text-primary-500 hover:scale-110 footerSocialLink hover:text-black transition duration-300'
+                            href={item.href} target='_blank'>
+                            {item.icon}
+                        </Link>
+                    </span>
+                ))
+                }
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
