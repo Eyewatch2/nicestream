2e84498003f3bf1ad8440c21eddc8903dfe000f0
PolCarva <pablocarvalhogimenez@gmail.com>
2024-05-16 17:18:24 -0300

    refactor: Update metadata description for SEO optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const archivo = Archivo({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nicestream | Home", 
+  description: "Nicestream es la principal red de oficinas especializadas en Social Big Data en Latinoamérica y España. Con más de 7 años de experiencia, nos destacamos en estrategias de marca, investigación, innovación y creación de contenido."
 };
 
 export default function RootLayout({
